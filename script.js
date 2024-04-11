c8170b9f33ad3572b3ea30bd52aeda2ca00b2809
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-04-11 21:00:37 +0530

    version 1.2

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector(".container");
+const optionsContainer = document.querySelector(".options-container");
+
+const country = "lk";
+const options = [
+  "general",
+  "entertainment",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
+
+
+let requestURL;
+
+const generateUI = (articles) => {
+  for (let item of articles) {
+    let card = document.createElement("div");
+    card.classList.add("news-card");
+    card.innerHTML = `<div class="news-image-container">
+    <img src="${item.urlToImage || "./newspaper.jpg"}" alt="" />
+    </div>
+    <div class="news-content">
+      <div class="news-title">
+        ${item.title}
+      </div>
+      <div class="news-description">
+      ${item.description || item.content || ""}
+      </div>
+      <a href="${item.url}" target="_blank" class="view-button">Read More</a>
+    </div>`;
+    container.appendChild(card);
+  }
+};
